67eb16fa0525bc4fd518e9d5511910711def9d5b
hesamyazdani <Hesam.YazdaniNezhad@Snapp.Supply>
2024-07-23 15:57:52 +0330

    Fix(Landing): Handle style of footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { routeList } from '@routes/routeList';
 import tapsiLogo from '@static/images/staticImages/tapsi-logo.png';
 import firstSlidePNG from '@static/images/staticImages/first-slide.png';
 import secondSlidePNG from '@static/images/staticImages/second-slide.png';
+import enemadPNG from '@static/images/staticImages/enemad.png';
 import NextLink from '@com/_core/NextLink';
 
 const Landing = () => {
@@ -36,12 +37,12 @@ const Landing = () => {
     <div className="flex flex-col items-center justify-center gap-3 w-full bg-primary-100 h-[calc(100vh-56px)]">
       <h1 className="text-[28px] font-semibold">تپسی دارو پلتفرم ارسال دارو</h1>
       <h3 className="text-xl font-semibold">در سریع‌تـرین زمان داروهاتـو دریافت کـن</h3>
-      <div className="w-11/12 my-5">
+      <div className="w-9/12 my-5">
         <NextImage src={firstSlidePNG} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill
                    quality={20}/>
       </div>
       <NextLink href={routeList.loginRoute}>
-        <div className="flex gap-2 bg-primary rounded-md p-2.5 w-11/12 justify-center">
+        <div className="flex gap-2 bg-primary rounded-md p-2.5 w-11/12 justify-center mt-5">
           <span className="text-white font-medium">ثبت سفارش</span>
           <ArrowLeftIconOutline width={24} height={24} fill={'white'}/>
         </div>
@@ -52,12 +53,14 @@ const Landing = () => {
       <h2 className="text-[28px] font-semibold">اگر داروخانه دارید...</h2>
       <h3 className="w-10/12 text-center">با ثبت‌نـام در تپســی دارو، به میلیون‌هــا کاربر دسترسی پیدا کنید و به
         جمع داروخانه‌های ما بپیوندید.</h3>
-      <div className="w-full my-5">
-        <NextImage src={secondSlidePNG} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill
+      <div className="w-9/12 my-5">
+        <NextImage src={secondSlidePNG}
+                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                   fill
                    quality={20}/>
       </div>
       <NextLink href={routeList.loginRoute}>
-        <div className="flex gap-2 bg-primary rounded-md p-2.5 w-11/12 justify-center">
+        <div className="flex gap-2 bg-primary rounded-md p-2.5 w-11/12 justify-center mt-5">
           <span className="text-white font-medium">ثبت‌نام داروخانه</span>
           <ArrowLeftIconOutline width={24} height={24} fill={'white'}/>
         </div>
@@ -68,15 +71,21 @@ const Landing = () => {
 
     </div>
 
-    <div className='bg-[#1E1E1E] py-6 px-4 text-white font-light'>
+    <div className='bg-[#1E1E1E] py-6 px-4 text-white font-light text-xs leading-6'>
       <NextImage src={tapsiLogo} height={20} width={85}/>
 
-      <p className='py-5'>تپسی دکتر، پلتفرم تهیه سریع و آسان داروهای مورد نیاز است. با استفاده از خدمات ما، داروهای خود را از نزدیک ترین داروخانه به سرعت و با اطمینان درب منزل دریافت کنید. تیم پشتیبانی ما همواره آماده پاسخگویی به سوالات و رفع نیازهای شماست.</p>
+      <p className='py-5 text-justify'>تپسی دکتر، پلتفرم تهیه سریع و آسان داروهای مورد نیاز است. با استفاده از خدمات ما، داروهای خود را از نزدیک ترین داروخانه به سرعت و با اطمینان درب منزل دریافت کنید. تیم پشتیبانی ما همواره آماده پاسخگویی به سوالات و رفع نیازهای شماست.</p>
 
-      <div className='flex justify-between items-start'>
-        <p className='whitespace-pre-line w-full pl-24'>آدرس: تهران، سعادت‌آباد، بالاتر از میدان کاج، بلوار بهزاد، نبش کوچه باغستان، پلاک ۲
-          کد پستی: ۱۹۹۸۶۳۵۸۲۰</p>
-        <div className='w-[86px] h-[86px] bg-white rounded-2xl'></div>
+      <div className='grid grid-cols-6 justify-between items-start'>
+        <div className='col-span-4'>
+          <p>آدرس: تهران، سعادت‌آباد، بالاتر از میدان کاج، بلوار بهزاد، نبش کوچه باغستان، پلاک ۲</p>
+          <p>کد پستی: ۱۹۹۸۶۳۵۸۲۰</p>
+        </div>
+        <div className='col-span-2 flex justify-end items-end h-full'>
+          <span className='p-3 bg-white rounded-2xl flex'>
+            <NextImage src={enemadPNG} height={61} width={58}/>
+          </span>
+        </div>
       </div>
 
     </div>
