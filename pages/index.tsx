16a195ae25c7dfb37a7e9ce06e13ded404784dee
Dariush Badakhshan <dariush.badakhshan@gmail.com>
2024-07-31 16:31:30 +0330

    feat(order): change logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { useGetProfile } from '@api/user/user.rq';
 import Spinner from '@com/_atoms/Spinner';
 import OrderRegisterSteps from '@com/_organisms/OrderRegisterSteps';
@@ -5,12 +7,25 @@ import MainLayout from '@com/_template/MainLayout';
 
 const HomePage = () => {
   const { data, isLoading: profileDataLoding } = useGetProfile();
-  const headerChildrenElement = <div className='text-[#ff5722] text-2xl font-bold'>TAPSI <span className='text-teal-600'>Doctor</span></div>
+  const headerChildrenElement = (
+    <Image
+      alt="logo"
+      src="/static/images/staticImages/logo.svg"
+      width={150}
+      height={30}
+    />
+  );
   return (
-    <MainLayout headerChildren={headerChildrenElement} className="px-6" title="ثبت سفارش">
-      {profileDataLoding === false ? <OrderRegisterSteps data={data} /> :
-        <Spinner className='h-[calc(100vh-180px)] w-full flex justify-center items-center' />
-      }
+    <MainLayout
+      headerChildren={headerChildrenElement}
+      className="px-6"
+      title="ثبت سفارش"
+    >
+      {profileDataLoding === false ? (
+        <OrderRegisterSteps data={data} />
+      ) : (
+        <Spinner className="h-[calc(100vh-180px)] w-full flex justify-center items-center" />
+      )}
     </MainLayout>
   );
 };
