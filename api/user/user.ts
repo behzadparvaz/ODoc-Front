a1c2425d298940b0ef5abfe92b9d7597702e94a4
Nouri.Shima <Nouri.Shima@Okala.com>
2024-08-02 15:20:09 +0330

    add photo uploader

--- api/user/user.ts
+++ api/user/user.ts
@@ -1,11 +1,27 @@
-import request from "@api/request";
+import request from '@api/request';
 
-export const AddLocation = async (body) => await request.post(`/Location/AddLocation`, body, { returnError: true });
-export const GetUserLocations = async () => await request.get(`/Location/GetLocations`);
-export const DeleteUserLocations = async (body) => await request.post(`/Location/DeleteLocation`, body);
+export const AddLocation = async (body) =>
+  await request.post(`/Location/AddLocation`, body, { returnError: true });
+
+export const GetUserLocations = async () =>
+  await request.get(`/Location/GetLocations`);
+
+export const DeleteUserLocations = async (body) =>
+  await request.post(`/Location/DeleteLocation`, body);
+
 export const GetProfile = async () => await request.get(`/Profile/GetProfile`);
-export const AddProfileInfo = async (body) => await request.post(`/Profile/AddProfile`, body);
-export const UpdateProfileInfo = async (body) => await request.post(`/Profile/UpdateProfile`, body);
-export const AddFamilyMembers = async (body) => await request.post(`/Profile/AddFamilyMembers`, body);
-export const UserSetPassword = async (body) => await request.put(`/user/SetPassword`, body);
-export const GetProfileRelation = (): Promise<any> => request.get(`/Profile/GetRelation`);
+
+export const AddProfileInfo = async (body) =>
+  await request.post(`/Profile/AddProfile`, body, { isFormData: true });
+
+export const UpdateProfileInfo = async (body) =>
+  await request.post(`/Profile/UpdateProfile`, body, { isFormData: true });
+
+export const AddFamilyMembers = async (body) =>
+  await request.post(`/Profile/AddFamilyMembers`, body);
+
+export const UserSetPassword = async (body) =>
+  await request.put(`/user/SetPassword`, body);
+  
+export const GetProfileRelation = (): Promise<any> =>
+  request.get(`/Profile/GetRelation`);
