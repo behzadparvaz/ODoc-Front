cda313415bd2ab00b267b72a7a0007791bfdb7b3
Dariush Badakhshan <dariush.badakhshan@gmail.com>
2024-08-10 13:17:30 +0330

    feat(asdress): limit location select to the tehran city

--- api/map/mapApis.ts
+++ api/map/mapApis.ts
@@ -16,15 +16,17 @@ interface IReverseLocationBody<T> {
 export const parsiMapSearchAddress = async (body: ISearchBody<string>) => {
   delete axios.defaults.headers.common['Authorization'];
   const data = await axios.get(
-    `${PARSI_API_URL}geocode/forward?district=${body.district}&key=${parsiMapAccessToken}&search_text=${body.search_text}&only_in_district=false`
+    `${PARSI_API_URL}geocode/forward?district=${body.district}&key=${parsiMapAccessToken}&search_text=${body.search_text}&only_in_district=false`,
   );
   return await data?.data;
 };
 
-export const parsiMapReverseLocation = async (body: IReverseLocationBody<string>) => {
+export const parsiMapReverseLocation = async (
+  body: IReverseLocationBody<string>,
+) => {
   delete axios.defaults.headers.common['Authorization'];
   const data = await axios.get(
-    `${PARSI_API_URL}geocode/reverse?key=${parsiMapAccessToken}&location=${body.location}&plate=true`
+    `${PARSI_API_URL}geocode/reverse?key=${parsiMapAccessToken}&location=${body.location}&plate=true&subdivision=true&local_address=true&approx_address=true`,
   );
   return await data?.data;
 };
