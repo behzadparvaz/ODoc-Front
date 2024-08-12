4fd694106143dfab6679292cb6ee411782cbab71
Dariush Badakhshan <dariush.badakhshan@gmail.com>
2024-08-12 16:49:13 +0330

    feat(design-system): add components export to index file

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,3 +1,15 @@
-import './tailwindStyle.css'
+import './tailwindStyle.css';
 
-export * from "./src/Box";
+export * from './src/Box';
+export * from './src/Button';
+export * from './src/Spinner';
+export * from './src/Calender';
+export * from './src/CheckBox';
+export * from './src/Radio';
+export * from './src/Select';
+export * from './src/TextInput';
+export * from './src/TextAreaInput';
+export * from './src/Switch';
+
+export * from './src/theme';
+export * from './src/icons';
