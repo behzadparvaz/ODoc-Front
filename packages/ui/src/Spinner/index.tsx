b4a7235300ff62f87d673c7054eb291239ae22f9
Dariush Badakhshan <dariush.badakhshan@gmail.com>
2024-08-12 16:05:36 +0330

    feat(design-system): add spinner component

---
+++ packages/ui/src/Spinner/index.tsx
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+
+type SpinnerProps = {
+  className?: string;
+  width?: string;
+  height?: string;
+  color?: string;
+};
+export const Spinner = ({
+  className,
+  color = 'bg-teal-300',
+  width = 'w-3.5',
+  height = 'h-3.5',
+}: SpinnerProps) => {
+  return (
+    <div
+      className={classNames(
+        'flex justify-center items-center min-w-8',
+        className,
+      )}
+    >
+      <div className="spinner flex gap-x-2">
+        <div className={`${width} ${height} ${color} rounded-full`} />
+        <div className={`${width} ${height} ${color} rounded-full`} />
+        <div className={`${width} ${height} ${color} rounded-full`} />
+      </div>
+    </div>
+  );
+};
