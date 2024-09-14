9214742277a711f28c9b691e4024ee26bfaab017
Raoufian.Masoud <Raoufian.Masoud@Okala.com>
2024-09-14 18:19:06 +0330

    product slider

--- pages/app/product/[categoryName].tsx
+++ pages/app/product/[categoryName].tsx
@@ -1,7 +1,26 @@
+import Button from '@com/_atoms/Button';
+import ProductSlider from '@com/_molecules/ProductSlider';
+import { ArrowRightIconOutline } from '@com/icons';
+import {
+  mobileModeMaxWidthClassName,
+  shouldShowMobileMode,
+} from '@configs/ControlMobileView';
 import { useRouter } from 'next/router';
 
 const Product = () => {
-  const { query } = useRouter();
-  return <>{query?.categoryName}</>;
+  const { query, push, back } = useRouter();
+
+  return (
+    <div
+      className={`${shouldShowMobileMode ? mobileModeMaxWidthClassName + ' mx-auto' : ''} min-h-screen bg-grey-50`}
+    >
+      <Button handleClick={back}>
+        <ArrowRightIconOutline fill="#000" width={24} height={24} />
+      </Button>
+      <div className="px-4">
+        <ProductSlider className="px-4 bg-white rounded-2xl py-6" />
+      </div>
+    </div>
+  );
 };
 export default Product;
