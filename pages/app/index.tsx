298cffbb7dfae55c9a769d589297af5f54b1d1e9
Nouri.Shima <Nouri.Shima@Okala.com>
2024-09-17 18:26:32 +0330

    remove landing page

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -2,6 +2,8 @@ import { useGetCarousels, useGetBanners } from '@api/promotion/promotion.rq';
 import Banner from '@com/_molecules/Banner';
 import MainPageLayout from '@com/_template/MainPageLayout';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from 'react';
 
 const MainSlider = dynamic(() => import('@com/_molecules/MainSlider'));
 const SearchBox = dynamic(() => import('@com/_atoms/SearchBox'));
@@ -10,6 +12,8 @@ const SectionTitle = dynamic(() => import('@com/_molecules/SectionTitle.nd'));
 const CarouselLine = dynamic(() => import('@com/_molecules/CarouselLine'));
 
 const HomePage = () => {
+  const { query } = useRouter();
+  const [showLayout, setShowLayout] = useState<boolean>(false);
   const { data: bannerData } = useGetBanners();
   const { data: carouselsData, isLoading } = useGetCarousels();
   const getCarouselDataData = (position: number) => {
@@ -18,42 +22,58 @@ const HomePage = () => {
     )?.[0];
     return carouselData;
   };
+  const isFromTapsi = query?.utm_source && query?.utm_source === 'TAPSI';
+  const url = isFromTapsi
+    ? 'https://accounts.tapsi.ir/login?client_id=doctor.tapsi&redirect_uri=https://tapsi.doctor/app&response_type=code&scope=tapsidoctor_access&prompt=none'
+    : null;
+  const tapsiLinkRef = useRef(null);
+  useEffect(() => {
+    url ? tapsiLinkRef?.current?.click() : null;
+    setTimeout(() => {
+      setShowLayout(true);
+    }, 500);
+  }, [url]);
 
   return (
-    <MainPageLayout
-      hasBottomNavigation
-      hasFooter
-      hasAddress
-      hasSearchIcon={false}
-    >
-      <div className="px-4">
-        <SearchBox className="px-4 my-2" />
-      </div>
-      <div className="my-4 px-2">
-        <Categories isHomePage />
-      </div>
-      {/* <div className="flex justify-between items-center border-b pb-2 mb-4">
+    <>
+      <a href={url} ref={tapsiLinkRef} className="hidden" />
+      {showLayout ? (
+        <MainPageLayout
+          hasBottomNavigation
+          hasFooter
+          hasAddress
+          hasSearchIcon={false}
+        >
+          <div className="px-4">
+            <SearchBox className="px-4 my-2" />
+          </div>
+          <div className="my-4 px-2">
+            <Categories isHomePage />
+          </div>
+          {/* <div className="flex justify-between items-center border-b pb-2 mb-4">
           <h2 className="text-lg font-bold">سفارشهای جاری</h2>
           <a href="#" className="text-blue-500">
           بیشتر
           </a>
         <OrderTracking />
         </div> */}
-      <MainSlider
-        className="py-2 px-4"
-        data={[bannerData?.queryResult?.[0], bannerData?.queryResult?.[1]]}
-      />
-      <CarouselLine data={getCarouselDataData(1)} className="my-4" />
-      <CarouselLine data={getCarouselDataData(2)} className="my-4" />
-      <Banner
-        style={{
-          background: 'linear-gradient(180deg, #FFFFFF 0%, #F5F7F7 100%)',
-        }}
-        className="px-4 py-6"
-        data={[bannerData?.queryResult?.[2]]}
-      />
-      <CarouselLine data={getCarouselDataData(3)} className="my-4" />
-    </MainPageLayout>
+          <MainSlider
+            className="py-2 px-4"
+            data={[bannerData?.queryResult?.[0], bannerData?.queryResult?.[1]]}
+          />
+          <CarouselLine data={getCarouselDataData(1)} className="my-4" />
+          <CarouselLine data={getCarouselDataData(2)} className="my-4" />
+          <Banner
+            style={{
+              background: 'linear-gradient(180deg, #FFFFFF 0%, #F5F7F7 100%)',
+            }}
+            className="px-4 py-6"
+            data={[bannerData?.queryResult?.[2]]}
+          />
+          <CarouselLine data={getCarouselDataData(3)} className="my-4" />
+        </MainPageLayout>
+      ) : null}
+    </>
   );
 };
 export default HomePage;
