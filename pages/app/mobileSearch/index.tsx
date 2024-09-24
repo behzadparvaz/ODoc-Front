95f4950c0e4987bd36a7beb2cb6648cd35bdd7d8
Dariush.b <dariush.badakhshan@gmail.com>
2024-09-24 20:07:52 +0330

    feat(search): change search to search on typing

--- pages/app/mobileSearch/index.tsx
+++ pages/app/mobileSearch/index.tsx
@@ -1,30 +1,26 @@
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { mobileSearchTexts } from '@com/texts/mobileSearchText';
 import { routeList } from '@routes/routeList';
 import { MainLayout } from '@com/Layout';
+import ProductList from '@com/_organisms/ProductList';
+import { ChevronLeftIconOutline } from '@com/icons';
 
-const EmptyContent = dynamic(() => import('@com/_atoms/EmptyContent'));
 const SearchBox = dynamic(() => import('@com/_atoms/SearchInput'));
-const PapularSearch = dynamic(() => import('@com/_molecules/PapularSearch'));
-const SearchSuggestion = dynamic(
-  () => import('@com/_molecules/SearchSuggestion'),
-);
-const Button = dynamic(() => import('@com/_atoms/Button'));
+const ScrollSlider = dynamic(() => import('@com/_molecules/ScrollSlider.nd'));
+const SectionTitle = dynamic(() => import('@com/_molecules/SectionTitle.nd'));
+
+const mockData = [
+  { text: 'ژلوفن', id: 0 },
+  { text: 'استامینوفن', id: 1 },
+  { text: 'ستیریزین', id: 2 },
+];
 
 const MobileSearch = () => {
-  const { back, query, push } = useRouter();
-  const searchTextQuery: string = query?.search_text
-    ? String(query?.search_text)
-    : null;
+  const { push } = useRouter();
 
   const [searchText, setSearchText] = useState<string>('');
-  useEffect(() => {
-    if (searchTextQuery !== null) {
-      setSearchText(searchTextQuery);
-    }
-  }, [query]);
+
   const handleSearchByImage = (e) => {
     push(`${routeList?.searchByImage}/${1236}`);
   };
@@ -32,7 +28,7 @@ const MobileSearch = () => {
   const handleGetSearchSuggestion = (value) => {
     setSearchText(value);
   };
-  const showResult = searchText?.length >= 2;
+
   return (
     <MainLayout
       hasHeader
@@ -42,53 +38,32 @@ const MobileSearch = () => {
       searchSection={
         <SearchBox
           defualtValue={searchText}
-          handleChange={(value) =>
-            value !== undefined && handleGetSearchSuggestion(value)
-          }
+          handleChange={(value) => handleGetSearchSuggestion(value)}
         />
       }
     >
-      <div className="w-full">
-        <div className="flex relative pl-4 pr-3 items-center gap-x-2">
-          {searchText?.length ? (
-            <Button
-              variant="primary"
-              className="absolute left-6 !px-1.5 hidden"
-              size="small"
-              buttonType="contained"
-              handleClick={(e) => e.prventDefault}
-            >
-              <input
-                onChange={(e) => handleSearchByImage(e)}
-                type="file"
-                className="absolute opacity-0 inset-0 w-full h-full"
-                accept="image/*;capture=camera"
-              />
-              {mobileSearchTexts?.searchByImage}
-            </Button>
-          ) : null}
-        </div>
-        {/* {showResult ? (
-          <p className="flex gap-x-1 items-center text-black text-sm mt-4 px-4">
-            <SearchIconOutline height={24} width={24} fill={'#000'} />
-            {mobileSearchTexts?.searchFor}
-            <span className="font-semibold">«{searchText}»</span>
-          </p>
-        ) : null} */}
-      </div>
-      {/* {showResult && (
-        <SearchSuggestion className="pt-1" searchText={searchText} />
-      )} */}
+      <SectionTitle
+        className="px-4 mb-2"
+        titleClassName="font-bold"
+        title="جستجوهای پرطرفدار"
+      />
 
-      {searchText?.length < 2 && (
-        <>
-          <PapularSearch className="mt-6" />
-          <EmptyContent
-            imgSrc="/static/images/staticImages/search-empty-content.png"
-            title={mobileSearchTexts?.noSearch}
-          />
-        </>
-      )}
+      <ScrollSlider className="gap-x-2 px-4">
+        {mockData?.map((item) => {
+          return (
+            <div
+              key={item?.id}
+              onClick={() => setSearchText(item?.text)}
+              className="flex justify-center items-center px-3 py-1 rounded-full border border-grey-100 text-black text-sm cursor-pointer"
+            >
+              {item?.text}
+              <ChevronLeftIconOutline width={24} height={24} fill="#000" />
+            </div>
+          );
+        })}
+      </ScrollSlider>
+
+      {searchText?.length >= 2 && <ProductList searchTerm={searchText} />}
     </MainLayout>
   );
 };
