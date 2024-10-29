7058a8d8ac539510b34976d0a4a503c49cec788d
Ali Bakhshandeh <a.bakhshandeh@email.com>
2024-10-29 17:13:36 +0330

    [feat]: added chips for slangs

--- components/_organisms/ProductList.tsx
+++ components/_organisms/ProductList.tsx
@@ -79,7 +79,7 @@ const ProductList = ({ searchTerm }: Props) => {
         <div className="p-4 space-y-4">
           {items?.length ? (
             items?.map((product, index) => (
-              <div>
+              <div key={index}>
                 <HorizontalProductCard
                   key={index}
                   prInfo={{
