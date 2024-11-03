ee6e05d6bfd2641ed0eb9e54611c00b88ae5caef
Ali Bakhshandeh <a.bakhshandeh@email.com>
2024-11-03 16:42:31 +0330

    [fix]: twoActionHorizontal for basket page

--- containers/basket/basket/index.tsx
+++ containers/basket/basket/index.tsx
@@ -208,7 +208,7 @@ const Page = () => {
         )}
       </div>
 
-      <ActionBar type="twoActionVertical" hasDivider={products.length > 0}>
+      <ActionBar type="twoActionHorizontal" hasDivider={products.length > 0}>
         {(basket?.products?.length > 0 || basket?.refrenceNumber) &&
           !draftData && (
             <>
