f009f01e7314cc481817caeae29ea19fd6f7b160
Ali Bakhshandeh <a.bakhshandeh@email.com>
2024-11-09 19:18:30 +0330

    [fix]: back button

--- containers/supplement/product/[productId].tsx
+++ containers/supplement/product/[productId].tsx
@@ -145,7 +145,7 @@ const SupplementProductContainer = () => {
       rightIcon={
         <div
           className="w-10 h-10 flex justify-center items-center cursor-pointer"
-          onClick={() => router.push(routeList.homeRoute)}
+          onClick={() => router.back()}
         >
           <CloseIconOutline width={32} height={42} stroke={colors.black} />
         </div>
