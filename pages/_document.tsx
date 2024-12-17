29fb0afa3b647c14491086d4b464e919e730333f
Ali Bakhshandeh <a.bakhshandeh@email.com>
2024-12-17 12:20:04 +0330

    added script tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,13 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-const G_ID = 'G-NK3MD5P0TQ';
+const GTM_ID = 'GTM-WTGQQVQW';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${G_ID}`}
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${G_ID}');`,
-            }}
-          />
+          <GoogleTagManager gtmId={GTM_ID} />
         </Head>
         <body>
           <Main />
