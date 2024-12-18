20fa02f1b9be386690930a9ef72c4ea5cfc01744
Ali Bakhshandeh <a.bakhshandeh@email.com>
2024-12-18 11:24:59 +0330

    fix sso

--- components/_atoms/loginWithSSO.tsx
+++ components/_atoms/loginWithSSO.tsx
@@ -1,5 +1,4 @@
 import { useLoginWithTapsiSSO } from '@api/user/user.rq';
-import useStorage from '@hooks/useStorage';
 import { routeList } from '@routes/routeList';
 import { searchParamToObject } from '@utilities/queryBuilder';
 import { useRouter } from 'next/router';
@@ -10,10 +9,8 @@ interface QueryParams {
 }
 
 const LoginWithSSO = () => {
-  const { getItem } = useStorage();
   const { replace } = useRouter();
   const { mutate } = useLoginWithTapsiSSO();
-  const token = getItem('token', 'local');
 
   const handleRedirects = async () => {
     const query: QueryParams = searchParamToObject(
@@ -21,24 +18,23 @@ const LoginWithSSO = () => {
     ) as QueryParams;
     const codeVendorSSO = query.code;
 
-    if (!token) {
-      if (codeVendorSSO) {
-        try {
-          await mutate(
-            { code: codeVendorSSO },
-            {
-              onSuccess: () => {
-                console.log('SSO login success');
-                return replace(routeList.homeRoute);
-              },
+    if (codeVendorSSO) {
+      try {
+        await mutate(
+          { code: codeVendorSSO },
+          {
+            onSuccess: () => {
+              console.log('SSO login success');
+              return replace(routeList.homeRoute, { query: {} });
             },
-          );
-        } catch (error) {
-          console.error('SSO login failed');
-          return replace(routeList.homeRoute);
-        }
+          },
+        );
+      } catch (error) {
+        console.error('SSO login failed');
+        return replace(routeList.homeRoute, { query: {} });
       }
     }
+
     return null;
   };
 
