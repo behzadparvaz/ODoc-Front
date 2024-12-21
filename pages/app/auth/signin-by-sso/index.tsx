03276a4b2ab215959c059a5d25bd0bd1262e8943
Ali Bakhshandeh <a.bakhshandeh@email.com>
2024-12-21 20:11:49 +0330

    added full sso

--- pages/app/auth/signin-by-sso/index.tsx
+++ pages/app/auth/signin-by-sso/index.tsx
@@ -1,57 +1,21 @@
 import { useLoginWithTapsiSSO } from '@api/user/user.rq';
 import NextImage from '@com/_core/NextImage';
 import { routeList } from '@routes/routeList';
+import { searchParamToObject } from '@utilities/queryBuilder';
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
+
 interface QueryParams {
   code?: string;
 }
 
-const SSOPage = () => {
+const RedirectPage = () => {
   const { mutate } = useLoginWithTapsiSSO();
-  const tapsiLinkRef = useRef<HTMLAnchorElement | null>(null);
+  const { replace } = useRouter();
 
-  const Config = {
-    baseUrl: 'https://accounts.tapsi.ir/',
-    clientId: 'doctor.tapsi',
-    redirectUri: 'https://tapsi.doctor/app/auth/signin-by-sso',
-    responseType: 'code',
-    ssoRedirect: true,
-    scope: 'tapsidoctor_access',
-    prompt: 'none',
-  };
+  const handleRedirects = async (query: QueryParams) => {
+    const codeVendorSSO = query.code;
 
-  const url = `${Config.baseUrl}login?sso_redirect=${Config.ssoRedirect}&client_id=${Config.clientId}&redirect_uri=${encodeURIComponent(Config.redirectUri)}&response_type=${Config.responseType}&scope=${Config.scope}&prompt=${Config.prompt}`;
-
-  const searchParamToObject = (searchParam: string) => {
-    if (typeof searchParam !== 'string') {
-      throw new TypeError('Expected a string as input');
-    }
-
-    try {
-      const pairs = searchParam.startsWith('?')
-        ? searchParam.slice(1).split('&')
-        : searchParam.split('&');
-
-      return pairs.reduce(
-        (accumulator, currentPair) => {
-          if (currentPair) {
-            const [key, value] = currentPair.split('=').map(decodeURIComponent);
-            accumulator[key] = value !== undefined ? value : null;
-          }
-          return accumulator;
-        },
-        {} as Record<string, string | null>,
-      );
-    } catch (error) {
-      console.error('Error parsing search parameters:', error);
-      return {};
-    }
-  };
-
-  const handleRedirects = async (query: any) => {
-    const codeVendorSSO = query.code as string | undefined;
-    console.log(codeVendorSSO);
     if (codeVendorSSO) {
       try {
         await mutate(
@@ -59,35 +23,30 @@ const SSOPage = () => {
           {
             onSuccess: () => {
               console.log('SSO login success');
-              // replace(routeList.homeRoute, { query: {} });
-              window.location.href = routeList.homeRoute;
+              replace(routeList.homeRoute);
+            },
+            onError: (error) => {
+              console.error('SSO login failed:', error);
+              replace(routeList.homeRoute);
             },
           },
         );
       } catch (error) {
-        console.error('SSO login failed');
-        // replace(routeList.homeRoute, { query: {} });
-        window.location.href = routeList.homeRoute;
+        console.error('Error during SSO login:', error);
+        replace(routeList.homeRoute);
       }
     }
   };
-
   useEffect(() => {
-    const query = searchParamToObject(window?.location?.search);
-    const isFromTapsi = query.utm_source === 'TAPSI';
+    const query: QueryParams = searchParamToObject(window.location.search);
 
     if (query.code) {
       handleRedirects(query);
-    }
-
-    if (isFromTapsi && tapsiLinkRef.current) {
-      tapsiLinkRef.current.click();
     }
   }, []);
 
   return (
     <div className="w-svh h-svh flex flex-col items-center justify-center bg-surface-primary">
-      <a href={url} ref={tapsiLinkRef} className="hidden" />
       <div
         role="status"
         className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
@@ -104,4 +63,4 @@ const SSOPage = () => {
   );
 };
 
-export default SSOPage;
+export default RedirectPage;
