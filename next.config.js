fd653a3b1f8faa52bcd551aa166d13eb542906f3
Ali Bakhshandeh <a.bakhshandeh@email.com>
2024-12-23 00:07:26 +0330

    fix issue workbox

--- next.config.js
+++ next.config.js
@@ -3,36 +3,66 @@ const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-const packageJson = require('./package.json');
-const { strict } = require('assert');
+const { version: application_version } = require('./package.json');
 
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   scope: '/',
   disable: process.env.NODE_ENV === 'development',
-  cacheOnFrontEndNav: true, // Enable caching for front-end navigation
-  aggressiveFrontEndNavCaching: true, // Aggressive caching for smoother navigation
-  reloadOnOnline: true, // Force reload when going back online
-  runtimeCaching: [
-    {
-      urlPattern: /\.(?:js|css|html|svg|png|jpg|jpeg|gif|webp)$/, // Static assets caching
-      handler: 'CacheFirst',
-      options: {
-        cacheName: `${packageJson.name}-static-resources`,
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 3 * 24 * 60 * 60, // Cache for 3 days
+  reloadOnOnline: true,
+  workboxOptions: {
+    mode: 'production',
+    clientsClaim: true,
+    skipWaiting: true,
+    cleanupOutdatedCaches: true,
+    disableDevLogs: true,
+    cacheId: `tapsi-doctor-web-app-${application_version}`,
+    runtimeCaching: [
+      {
+        urlPattern: /\.(jpg|jpeg|gif|png|svg|ico|webp)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'image-cache',
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 3 * 24 * 60 * 60,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
         },
       },
-    },
-    {
-      // Matches dynamic routes like /blog/:slug
-      urlPattern: /^\/[a-zA-Z0-9]+$/,
-      handler: 'NetworkOnly', // Always fetch from network
-    },
-  ],
-  disableDevLogs: true,
+      {
+        urlPattern: /\.(woff|woff2|ttf|otf)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'font-cache',
+          expiration: {
+            maxEntries: 30,
+            maxAgeSeconds: 3 * 24 * 60 * 60,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+      {
+        urlPattern: /\.(css)$/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'css-cache',
+          expiration: {
+            maxEntries: 30,
+            maxAgeSeconds: 3 * 24 * 60 * 60,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
 });
 
 const nextConfig = {
@@ -48,13 +78,14 @@ const nextConfig = {
     ],
     minimumCacheTTL: 60,
   },
-  webpack: (config) => {
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
     return config;
   },
+
   async headers() {
     return [
       {
