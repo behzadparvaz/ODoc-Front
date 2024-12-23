93460e28adab0791ce3a1d831d41fa3a974e46b7
Dariush.b <dariush.badakhshan@gmail.com>
2024-12-23 13:28:05 +0330

    TECH-910: make plaque and unit type to number

--- components/_organisms/AddressList.tsx
+++ components/_organisms/AddressList.tsx
@@ -1,7 +1,9 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import AddressItem from '@com/_atoms/AddressItem';
 import { RootState } from '@utilities/types';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { setUserAction } from '@redux/user/userActions';
+import useModal from '@hooks/useModal';
 
 type AddressListProps = {
   data: any;
@@ -14,16 +16,19 @@ const AddressList = ({
   handleClickItem,
   inOrderPage = false,
 }: AddressListProps) => {
+  const { removeLastModal } = useModal();
   const { user } = useSelector((state: RootState) => state.user);
   const defaultAddress = user?.defaultAddress;
-  // const handleClick = (item) => {
-  //   if (inOrderPage) {
-  //     handleClickItem(item);
-  //     setAddressSelected(item?.id);
-  //   } else {
-  //     return null;
-  //   }
-  // };
+  const dispatch = useDispatch();
+
+  const handleClickAddress = (item) => {
+    dispatch(
+      setUserAction({
+        defaultAddress: item,
+      }),
+    ),
+      removeLastModal();
+  };
 
   return (
     <div className="w-full ">
@@ -31,9 +36,9 @@ const AddressList = ({
         const activeItem = defaultAddress?.id === item?.id;
         return (
           <>
-            {/* <div key={item?.id} onClick={(e) => handleClick(item)}> */}
-            <AddressItem activeItem={activeItem} addressInfo={item} />
-            {/* </div> */}
+            <div key={item?.id} onClick={(e) => handleClickAddress(item)}>
+              <AddressItem activeItem={activeItem} addressInfo={item} />
+            </div>
 
             <div className="w-full h-2 bg-grey-50" />
           </>
