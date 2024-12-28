c015c360fda2d0f08070b07d065a76495eb526b9
Dariush.b <dariush.badakhshan@gmail.com>
2024-12-28 19:07:22 +0330

    TECH-948: change render basket route and add delete RX item from basket

--- containers/basket/components/PrescriptionItem.tsx
+++ containers/basket/components/PrescriptionItem.tsx
@@ -1,36 +1,61 @@
+import {
+  useDeleteProductBasket,
+  useGetCurrentBasket,
+} from '@api/basket/basketApis.rq';
 import NextImage from '@com/_core/NextImage';
+import { colors } from '@configs/Theme';
 import specialPatients from '@public/images/tiles/nonPrescriptionMedicine.png';
 import prescriptionMedicine from '@public/images/tiles/prescriptionMedicine.png';
+import Icon from '@utilities/icon';
 
 interface NotOtcProps {
-  BasketRefrenceNumber: string;
-  BasketIsSpecialPatient: boolean;
+  refrenceNumber: string;
+  isSpecialPatient: boolean;
+  hasDivider?: boolean;
 }
 
 const PrescriptionItem = ({
-  BasketRefrenceNumber,
-  BasketIsSpecialPatient,
-}: NotOtcProps) => (
-  <>
-    <div className="w-full h-20 flex items-center justify-start gap-x-4">
-      <NextImage
-        src={BasketIsSpecialPatient ? specialPatients : prescriptionMedicine}
-        alt="rx-image"
-        width={72}
-        height={72}
-      />
-      <div className="flex flex-col gap-y-1">
-        <span className="text-base font-semibold">
-          {BasketIsSpecialPatient ? 'نسخه بیماری خاص' : 'دارو با نسخه'}
-        </span>
-        <span className="text-base font-semibold">{`کد نسخه ${BasketRefrenceNumber}`}</span>
-      </div>
-    </div>
+  refrenceNumber,
+  isSpecialPatient,
+  hasDivider,
+}: NotOtcProps) => {
+  const { refetch: refetchGetBasket } = useGetCurrentBasket({
+    refetchOnMount: 'false',
+  });
+  const { mutate: popProductOfCart } = useDeleteProductBasket({
+    onSuccess: () => {
+      refetchGetBasket();
+    },
+  });
 
-    <div className="w-full px-4">
-      <div className="w-full h-[1px] bg-grey-200" />
-    </div>
-  </>
-);
+  const handleDeleteItem = () => {
+    popProductOfCart({ type: 'RX', refrenceNumber: refrenceNumber });
+  };
+
+  return (
+    <>
+      <div className="w-full h-20 flex items-center justify-start gap-x-4">
+        <NextImage
+          src={isSpecialPatient ? specialPatients : prescriptionMedicine}
+          alt="rx-image"
+          width={72}
+          height={72}
+        />
+        <div className="w-full flex flex-col gap-y-1">
+          <span className="text-base font-semibold">
+            {isSpecialPatient ? 'نسخه بیماری خاص' : 'دارو با نسخه'}
+          </span>
+          <span className="text-base font-semibold">{`کد نسخه ${refrenceNumber}`}</span>
+        </div>
+
+        <div onClick={handleDeleteItem} className="cursor-pointer">
+          <Icon name="Trash" width={1.25} height={1.25} fill={colors?.black} />
+        </div>
+      </div>
+
+      {hasDivider && <div className="w-full h-[1px] bg-grey-200" />}
+    </>
+  );
+};
 
 export default PrescriptionItem;
