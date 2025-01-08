a164cf4bf37d5bddddb5b91f0b10b2651144d63e
Ali Bakhshandeh <a.bakhshandeh@email.com>
2025-01-08 15:15:29 +0330

    feat:[TECH-973]: added seach for croucels

--- api/promotion/promotion.ts
+++ api/promotion/promotion.ts
@@ -7,13 +7,12 @@ export const GetBanners = async () =>
 export const GetCarousels = async () =>
   await request.get(`/Promotion/GetCarousels`);
 
-  export const GetCarouselById = async (body: any) => {
-    const params = builder(body);
-    const res: any = await request.get(
-      `/Promotion/GetProductCarousel${params}`,
-    );
-    if (res?.statusCode === 200) {
-      return res?.value;
-    } else return res;
-  };
-  
+export const GetCarouselById = async (body: any) => {
+  const params = builder(body);
+  const res: any = await request.get(
+    `/Promotion/GetProductCarousel${params}`,
+  );
+  if (res?.statusCode === 200) {
+    return res?.value;
+  } else return res;
+};
--- containers/offer/components/products.tsx
+++ containers/offer/components/products.tsx
@@ -1,11 +1,13 @@
 import classNames from 'classnames';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { useGetCarouselProduct } from '@api/promotion/promotion.rq';
 import useProductNavigation from '@hooks/useNavigateToPdp';
+import SearchBox from '@com/_atoms/SearchInput';
+import { debounce } from '@utilities/debounce';
 
 const VerticalProductCardShimmer = dynamic(
   () => import('@com/_atoms/verticalProductCardShimmer'),
@@ -19,8 +21,18 @@ const Products = () => {
   const { query, push } = useRouter();
   const { ref, inView } = useInView();
   const { navigateToPdp } = useProductNavigation();
-  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useGetCarouselProduct({ carouselId: query?.carouselId });
+  const [searchInput, setSearchInput] = useState('');
+  const {
+    data,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    refetch,
+  } = useGetCarouselProduct({
+    carouselId: query?.carouselId,
+    search: searchInput,
+  });
 
   const productList = useMemo(
     () =>
@@ -42,67 +54,90 @@ const Products = () => {
     }
   }, [inView, fetchNextPage]);
 
-  if ((isLoading || isFetchingNextPage) && !productList?.length) {
-    return (
-      <div
-        className={classNames(
-          'h-full w-full grid grid-cols-2 overflow-y-scroll',
-        )}
-      >
-        {[...Array(8).keys()].map((item) => (
-          <div
-            key={item}
-            className="w-full flex justify-center border-border-primary first:!border-t [&:nth-child(2)]:border-t h-[217px] odd:border-r odd:border-l odd:border-b even:border-l even:border-b"
-          >
-            <VerticalProductCardShimmer />
-          </div>
-        ))}
-      </div>
-    );
-  }
+  useEffect(() => {
+    console.log(searchInput);
+    refetch();
+  }, [searchInput]);
 
-  return (
-    <div
-      className={classNames(
-        'h-max w-full grid grid-cols-2 overflow-y-scroll',
-        (data?.pages?.at(-1)?.pageNumber >= 10 || query?.pageNumber) &&
-          'mb-[86px]',
-      )}
-    >
-      {productList?.map((item) => (
-        <VerticalProductCard
-          onClick={() =>
-            navigateToPdp({ item, ProductTypeId: item.productType })
-          }
-          className="!h-[217px] border-border-primary odd:border odd:border-t-0 first:!border-t even:border-l even:border-b [&:nth-child(2)]:border-t"
-          productData={item}
-          key={item?.irc}
-          hasAddToCart
-        />
-      ))}
+  const handleSearchChange = (value: string) => {
+    setSearchInput(value);
+  };
 
-      {data?.pages?.at(-1)?.pageNumber < 10 && !query?.pageNumber && (
-        <div ref={ref} className="w-full col-start-1 col-end-3">
-          {hasNextPage && isFetchingNextPage && (
-            <div className="h-full w-full grid grid-cols-2 overflow-y-scroll">
-              {[...Array(8).keys()].map((item) => (
-                <div
-                  key={item}
-                  className="w-full flex justify-center border-border-primary h-[217px] odd:border-r odd:border-l odd:border-b even:border-l even:border-b"
-                >
-                  <VerticalProductCardShimmer />
+  const render = useMemo(() => {
+    if ((isLoading || isFetchingNextPage) && !productList?.length) {
+      return (
+        <div
+          className={classNames(
+            'h-full w-full grid grid-cols-2 overflow-y-scroll',
+          )}
+        >
+          {[...Array(8).keys()].map((item) => (
+            <div
+              key={item}
+              className="w-full flex justify-center border-border-primary first:!border-t [&:nth-child(2)]:border-t h-[217px] odd:border-r odd:border-l odd:border-b even:border-l even:border-b"
+            >
+              <VerticalProductCardShimmer />
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className={classNames(
+            'h-max w-full grid grid-cols-2 overflow-y-scroll',
+            (data?.pages?.at(-1)?.pageNumber >= 10 || query?.pageNumber) &&
+              'mb-[86px]',
+          )}
+        >
+          {productList?.map((item) => (
+            <VerticalProductCard
+              onClick={() =>
+                navigateToPdp({ item, ProductTypeId: item.productType })
+              }
+              className="!h-[217px] border-border-primary odd:border odd:border-t-0 first:!border-t even:border-l even:border-b [&:nth-child(2)]:border-t"
+              productData={item}
+              key={item?.irc}
+              hasAddToCart
+            />
+          ))}
+
+          {data?.pages?.at(-1)?.pageNumber < 10 && !query?.pageNumber && (
+            <div ref={ref} className="w-full col-start-1 col-end-3">
+              {hasNextPage && isFetchingNextPage && (
+                <div className="h-full w-full grid grid-cols-2 overflow-y-scroll">
+                  {[...Array(8).keys()].map((item) => (
+                    <div
+                      key={item}
+                      className="w-full flex justify-center border-border-primary h-[217px] odd:border-r odd:border-l odd:border-b even:border-l even:border-b"
+                    >
+                      <VerticalProductCardShimmer />
+                    </div>
+                  ))}
                 </div>
-              ))}
+              )}
             </div>
           )}
-        </div>
-      )}
 
-      {isShownPagination && (
-        <Pagination
-          lastPage={Math.floor(data?.pages?.[0]?.totalCount / 10) + 1}
+          {isShownPagination && (
+            <Pagination
+              lastPage={Math.floor(data?.pages?.[0]?.totalCount / 10) + 1}
+            />
+          )}
+        </div>
+      );
+    }
+  }, [data, searchInput, hasNextPage, isFetchingNextPage]);
+
+  return (
+    <div className="flex flex-col gap-y-lg">
+      <div className="flex w-full justify-center items-center p-4">
+        <SearchBox
+          handleChange={handleSearchChange}
+          defualtValue={searchInput}
         />
-      )}
+      </div>
+      {render}
     </div>
   );
 };
