a1d636fc88d92f1d06fd0db74b694a2ce965da04
Ali Bakhshandeh <a.bakhshandeh@email.com>
2025-01-11 13:01:24 +0330

    fix:[TECH-973]: fixed issue search in offer page

--- containers/offer/components/products.tsx
+++ containers/offer/components/products.tsx
@@ -1,13 +1,12 @@
 import classNames from 'classnames';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { useGetCarouselProduct } from '@api/promotion/promotion.rq';
 import useProductNavigation from '@hooks/useNavigateToPdp';
 import SearchBox from '@com/_atoms/SearchInput';
-import { debounce } from '@utilities/debounce';
 
 const VerticalProductCardShimmer = dynamic(
   () => import('@com/_atoms/verticalProductCardShimmer'),
@@ -21,18 +20,11 @@ const Products = () => {
   const { query, push } = useRouter();
   const { ref, inView } = useInView();
   const { navigateToPdp } = useProductNavigation();
-  const [searchInput, setSearchInput] = useState('');
-  const {
-    data,
-    isLoading,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    refetch,
-  } = useGetCarouselProduct({
-    carouselId: query?.carouselId,
-    search: searchInput,
-  });
+  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useGetCarouselProduct({
+      carouselId: query?.carouselId,
+      search: query?.search,
+    });
 
   const productList = useMemo(
     () =>
@@ -48,96 +40,91 @@ const Products = () => {
     [data?.pages?.at(-1)?.pageNumber, query?.pageNumber],
   );
 
+  const onChangeSearchInput = (value: string) => {
+    console.log(value);
+    if (value)
+      push({ query: { ...query, search: value } }, undefined, {
+        shallow: true,
+      });
+  };
+
   useEffect(() => {
     if (inView && hasNextPage) {
       fetchNextPage();
     }
   }, [inView, fetchNextPage]);
 
-  useEffect(() => {
-    console.log(searchInput);
-    refetch();
-  }, [searchInput]);
-
-  const handleSearchChange = (value: string) => {
-    setSearchInput(value);
-  };
-
-  const render = useMemo(() => {
-    if ((isLoading || isFetchingNextPage) && !productList?.length) {
-      return (
-        <div
-          className={classNames(
-            'h-full w-full grid grid-cols-2 overflow-y-scroll',
-          )}
-        >
-          {[...Array(8).keys()].map((item) => (
-            <div
-              key={item}
-              className="w-full flex justify-center border-border-primary first:!border-t [&:nth-child(2)]:border-t h-[217px] odd:border-r odd:border-l odd:border-b even:border-l even:border-b"
-            >
-              <VerticalProductCardShimmer />
-            </div>
-          ))}
-        </div>
-      );
-    } else {
-      return (
-        <div
-          className={classNames(
-            'h-max w-full grid grid-cols-2 overflow-y-scroll',
-            (data?.pages?.at(-1)?.pageNumber >= 10 || query?.pageNumber) &&
-              'mb-[86px]',
-          )}
-        >
-          {productList?.map((item) => (
-            <VerticalProductCard
-              onClick={() =>
-                navigateToPdp({ item, ProductTypeId: item.productType })
-              }
-              className="!h-[217px] border-border-primary odd:border odd:border-t-0 first:!border-t even:border-l even:border-b [&:nth-child(2)]:border-t"
-              productData={item}
-              key={item?.irc}
-              hasAddToCart
-            />
-          ))}
-
-          {data?.pages?.at(-1)?.pageNumber < 10 && !query?.pageNumber && (
-            <div ref={ref} className="w-full col-start-1 col-end-3">
-              {hasNextPage && isFetchingNextPage && (
-                <div className="h-full w-full grid grid-cols-2 overflow-y-scroll">
-                  {[...Array(8).keys()].map((item) => (
-                    <div
-                      key={item}
-                      className="w-full flex justify-center border-border-primary h-[217px] odd:border-r odd:border-l odd:border-b even:border-l even:border-b"
-                    >
-                      <VerticalProductCardShimmer />
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          )}
-
-          {isShownPagination && (
-            <Pagination
-              lastPage={Math.floor(data?.pages?.[0]?.totalCount / 10) + 1}
-            />
-          )}
-        </div>
-      );
-    }
-  }, [data, searchInput, hasNextPage, isFetchingNextPage]);
+  if ((isLoading || isFetchingNextPage) && !productList?.length) {
+    return (
+      <div
+        className={classNames(
+          'h-full w-full grid grid-cols-2 overflow-y-scroll',
+        )}
+      >
+        {[...Array(8).keys()].map((item) => (
+          <div
+            key={item}
+            className="w-full flex justify-center border-border-primary first:!border-t [&:nth-child(2)]:border-t h-[217px] odd:border-r odd:border-l odd:border-b even:border-l even:border-b"
+          >
+            <VerticalProductCardShimmer />
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   return (
-    <div className="flex flex-col gap-y-lg">
-      <div className="flex w-full justify-center items-center p-4">
-        <SearchBox
-          handleChange={handleSearchChange}
-          defualtValue={searchInput}
-        />
+    <div className="relative">
+      <div className="sticky top-0 z-10 bg-white border-b">
+        <div className="mx-4 mb-4">
+          <SearchBox
+            handleChange={onChangeSearchInput}
+            defualtValue={query?.search}
+          />
+        </div>
       </div>
-      {render}
+      <div
+        className={classNames(
+          'h-max w-full grid grid-cols-2 overflow-y-scroll',
+          (data?.pages?.at(-1)?.pageNumber >= 10 || query?.pageNumber) &&
+            'mb-[86px]',
+        )}
+      >
+        {productList?.map((item) => (
+          <VerticalProductCard
+            onClick={() =>
+              navigateToPdp({ item, ProductTypeId: item.productType })
+            }
+            className="!h-[217px] border-border-primary odd:border odd:border-t-0 first:!border-t even:border-l even:border-b"
+            productData={item}
+            key={item?.irc}
+            hasAddToCart
+          />
+        ))}
+
+        {data?.pages?.at(-1)?.pageNumber < 10 && !query?.pageNumber && (
+          <div ref={ref} className="w-full col-start-1 col-end-3">
+            {hasNextPage && isFetchingNextPage && (
+              <div className="h-full w-full grid grid-cols-2 overflow-y-scroll">
+                {[...Array(8).keys()].map((item) => (
+                  <div
+                    key={item}
+                    className="w-full flex justify-center border-border-primary h-[217px] odd:border-r odd:border-l odd:border-b even:border-l even:border-b"
+                  >
+                    <VerticalProductCardShimmer />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        )}
+
+        {isShownPagination && (
+          <Pagination
+            lastPage={Math.floor(data?.pages?.[0]?.totalCount / 10) + 1}
+          />
+        )}
+      </div>
     </div>
   );
 };
